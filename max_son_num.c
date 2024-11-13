4280df337ec3fa5e79c5471d0af4606f9e8863d3
GR-GRAPE <2024190506010@std.uestc.edu.cn>
2024-11-13 20:29:20 +0800

    Update max_son_num.c

--- max_son_num.c
+++ max_son_num.c
@@ -5,14 +5,3 @@ int gcd(int a, int b)
     if(b == 0) return a;
     return gcd(b, a % b);
 }
-
-int main()
-{
-    long long int a,b,num;
-    scanf("%d %d",&a,&b);
-    num = gcd(a,b);
-    
-    printf("%d",num);
-
-    return 0;
-}
